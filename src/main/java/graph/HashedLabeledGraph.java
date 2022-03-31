3f88a7e811031d2d80d33ae2a641df406df7f746
AlessandroPaparella <61472307+AlessandroPaparella@users.noreply.github.com>
2022-03-31 20:24:41 +0200

    abstract class for labeled graph implemented with map

---
+++ src/main/java/graph/HashedLabeledGraph.java
@@ -0,0 +1,73 @@
+package graph;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public abstract class HashedLabeledGraph<N, L> implements LabeledGraph<N, L> {
+	private Map<N, Map<N, L>> arcs = new HashMap<N, Map<N,L>>();
+	private Set<N> nodes= new HashSet<N>();
+
+	public HashedLabeledGraph() {
+		super();
+	}
+
+	public void insNode(N node) {
+		if(nodes.add(node)==true)
+			arcs.put(node, new HashSet<N>());
+	}
+
+	public Boolean contains(N node) {
+		// TODO Auto-generated method stub
+		return nodes.contains(node);
+	}
+
+	public Boolean isLabeled() {
+		return true;
+	}
+
+	public void delNode(N node) throws GraphException {
+		// TODO Auto-generated method stub
+
+	}
+
+	public Boolean isEmpty() {
+		return nodes.isEmpty();
+	}
+
+	public Collection<N> getAdjacent(N node) throws GraphException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Boolean containsArc(N node1, N node2) throws GraphException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Boolean isNull() {
+		Boolean isNull = true;
+		for(N n : arcs.keySet()) {
+			if(!arcs.get(n).isEmpty()) {
+				isNull = false;
+				break;
+			}
+		}
+		return isNull;
+	}
+
+	public Boolean isDirected() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public abstract void insArc(N node1, N node2, L label);
+
+	public abstract void removeArc(N node1, N node2) throws GraphException;
+
+	public abstract L readArc(N node1, N node2) throws GraphException;
+
+
+}
